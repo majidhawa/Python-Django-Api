131fb3a8af04da20c935d1462a94741406d50787
majidhawa <majidhawaah@gmail.com>
2024-08-19 23:04:45 +0300

    added minimal serializer

--- api/serializer.py
+++ api/serializer.py
@@ -6,42 +6,102 @@ from rest_framework import serializers
 from teacher.models import Teacher
 from datetime import datetime
 
+class CourseMinimalSerializer(serializers.ModelSerializer):
+    duration = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Courses
+        fields = ['id',  'course_name', 'course_code', 'duration']
+    
+    def get_duration(self, obj):
+      
+        start_date = obj.start_date
+        end_date = obj.end_date
+    
+        duration = end_date - start_date
+        return duration.days
+ 
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Courses
         fields = "__all__"
-    
+
+
+
 class StudentSerializer(serializers.ModelSerializer):
-    courses= CourseSerializer(many=True)
-    
+    courses = CourseSerializer(many=True)
+
     class Meta:
         model = Student
-        exclude=["email"]
+        exclude = ["email"]
+
 class MinimalStudentSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
     
-    full_name=serializers.SerializerMethodField()
-    def get_full_name(self,object):
+    def get_full_name(self, object):
         return f'{object.first_name} {object.last_name}'
     
-    age=serializers.SerializerMethodField()
-    def get_age(self,object):
-        today = datetime.now
-        age = today-object.date_of_birth
-        return age 
+    age = serializers.SerializerMethodField()
+    
+    def get_age(self, object):
+        today = datetime.now().date()
+        age = today.year - object.date_of_birth.year - ((today.month, today.day) < (object.date_of_birth.month, object.date_of_birth.day))
+        return age
+    
     class Meta:
         model = Student
-        fields = ["id","full_name","email","age"]
+  
+  
+        fields = ["id", "full_name", "email", "age"]
+
+class TeacherMinimalSerializer(serializers.ModelSerializer):
+    courses = serializers.SerializerMethodField()
+    full_name = serializers.SerializerMethodField()
+    
+    def get_full_name(self, object):
+        return f'{object.first_name} {object.last_name}'
+    def get_courses(self, obj):
+        courses = Courses.objects.filter(teacher=obj)
+        return [course.title for course in courses]
+    
+    class Meta:
+        model = Teacher
+        fields = ['id', 'full_name', 'email', 'courses']
+
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = "__all__"
+        
+        
+
+class ClassroomMinimalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Classroom
+        fields = ['id', 'class_name', 'description']   
+        
 
 class ClassroomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Classroom
         fields = "__all__"
+
+
+class ClassPeriodMinimalSerializer(serializers.ModelSerializer):
+    def get_duration(self, obj):
+        start_date = obj.start_date
+        end_date = obj.end_date
+        duration = end_date - start_date
+        return duration.days
+
+    class Meta:
+        model = Class_Period
+        fields = ['id', ' name', 'duration']      
+        
+             
 class Class_PeriodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Class_Period
         fields = '__all__'
+        
--- api/views.py
+++ api/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from api.serializer import Class_PeriodSerializer, CourseSerializer, ClassroomSerializer, StudentSerializer, TeacherSerializer,MinimalStudentSerializer
+from api.serializer import (
+    Class_PeriodSerializer, CourseSerializer, ClassroomSerializer, 
+    StudentSerializer, TeacherSerializer, MinimalStudentSerializer, 
+    CourseMinimalSerializer, TeacherMinimalSerializer, 
+    ClassroomMinimalSerializer, ClassPeriodMinimalSerializer
+)
 from classperiod.models import Class_Period
 from classroom.models import Classroom
 from course.models import Courses
@@ -8,7 +13,6 @@ from student.models import Student
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from teacher.models import Teacher
-# Create your views here.
 
 class StudentListViews(APIView):
     def get(self, request):
@@ -16,58 +20,66 @@ class StudentListViews(APIView):
         serializer= MinimalStudentSerializer(students,many=True)
         return Response(serializer.data)
 
+
 class TeacherListViews(APIView):
     def get(self, request):
         teacher = Teacher.objects.all()
-        serializer = TeacherSerializer(teacher, many=True)
+        serializer = TeacherMinimalSerializer(teacher, many=True)
         return Response(serializer.data)
+    
     def post(self, request):
-        serializer = TeacherSerializer(data=request.data)
+        serializer = TeacherMinimalSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.data)
+    
+    
 class CourseListViews(APIView):
     def get(self, request):
         course = Courses.objects.all()
-        serializer = CourseSerializer(course, many=True)
+        serializer = CourseMinimalSerializer(course, many=True)
         return Response(serializer.data)
+    
     def post(self, request):
-        serializer = CourseSerializer(data=request.data)
+        serializer = CourseMinimalSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.data)
+       
 class ClassroomListViews(APIView):
     def get(self, request):
         classroom = Classroom.objects.all()
-        serializer = ClassroomSerializer(classroom, many=True)
+        serializer = ClassroomMinimalSerializer(classroom, many=True)
         return Response(serializer.data)
+    
     def post(self, request):
-        serializer = ClassroomSerializer(data=request.data)
+        serializer = ClassroomMinimalSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.data)
+       
+       
 class Class_PeriodListViews(APIView):
     def get(self, request):
         class_period = Class_Period.objects.all()
-        serializer = Class_PeriodSerializer(class_period, many=True)
+        serializer = ClassPeriodMinimalSerializer(class_period, many=True)
         return Response(serializer.data)
+    
     def post(self, request):
-        serializer = Class_PeriodSerializer(data=request.data)
+        serializer = ClassPeriodMinimalSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.data) 
+        
+        
 class StudentDetailView(APIView):
     def get(self, request, id):
         student = Student.objects.get(id = id)
